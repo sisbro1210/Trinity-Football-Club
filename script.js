03730bc853dd8a550d3af64436102024c5668bf8
sisbro1210 <142943509+sisbro1210@users.noreply.github.com>
2023-08-31 12:55:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const button = document.getElementById("button")
+const body = document.body.classList
+var theme = document.documentElement.style
+var current = localStorage.getItem("current")
+
+    
+button.addEventListener("click", onClick)
+function onClick()
+{
+   
+    if (current == "dark"){
+    
+    theme.setProperty("--text-colourA", "#000000")
+    theme.setProperty("--text-colourB", " #ffffff")
+    theme.setProperty("--background-light", "#010142")
+    theme.setProperty("--background-colour", "#ffffff")
+    theme.setProperty("--background-dark", "#010142")
+    localStorage.setItem("current", "light")
+    location.reload()
+    
+    
+   }else if (current == "light"){
+    theme.setProperty("--text-colourA", "#ffffff")
+    theme.setProperty("--text-colourB", " #a4edff")
+    theme.setProperty("--background-light", "#140a27")
+    theme.setProperty("--background-colour", "#110723")
+    theme.setProperty("--background-dark", "#0e061c")
+    localStorage.setItem("current", "dark")
+    location.reload()
+    
+
+   }
+
+   
+
+   
+   
+   
+   
+}
+
+function onload() {
+    
+
+    if(current == "light") {
+        localStorage.setItem("current", "light")
+    
+    }else if (current == "dark") {
+        localStorage.setItem("current", "dark")
+    
+    }else if (current === null) {
+        localStorage.setItem("current", "light")
+    
+    }
+
+    if (current == "dark"){
+
+        theme.setProperty("--text-colourA", "#ffffff")
+        theme.setProperty("--text-colourB", " #a4edff")
+        theme.setProperty("--background-light", "#140a27")
+        theme.setProperty("--background-colour", "#110723")
+        theme.setProperty("--background-dark", "#0e061c")
+        
+        
+
+        }else if (current == "light") {
+          
+            theme.setProperty("--text-colourA", "#000000")
+            theme.setProperty("--text-colourB", " #ffffff")
+            theme.setProperty("--background-light", "#010142")
+            theme.setProperty("--background-colour", "#ffffff")
+            theme.setProperty("--background-dark", "#010142")
+        
+        }else{
+
+        }
+
+    }
